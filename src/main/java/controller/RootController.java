fbf9dc2bc98b5df49434d0f26b94126b48531258
Eduard Aleksandrov <eduard.ale@list.ru>
2025-10-28 20:22:47 +0300

    init

---
+++ src/main/java/controller/RootController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+    @GetMapping
+    @ResponseBody  // Optional if you are returning a view
+    public String home() {
+        return "Welcome to the Home Page!";
+    }
+}
